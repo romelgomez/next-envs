39227da844495890883446a83392012471340e17
romelgomez <bmxandcode@gmail.com>
2024-08-19 14:05:55 -0500

    chore: remove old imple

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-export default function Home({
-  NEXT_PUBLIC_EXAMPLE_ENV_VAR,
-  SECRET_KEY,
-}: {
-  NEXT_PUBLIC_EXAMPLE_ENV_VAR: string;
-  SECRET_KEY: string;
-}) {
+export default function Home() {
+
+  const NEXT_PUBLIC_EXAMPLE_ENV_VAR = process.env.NEXT_PUBLIC_EXAMPLE_ENV_VAR;
+  const SECRET_KEY = process.env.SECRET_KEY;
+
   return (
     <div>
       <h1>Experiment One</h1>
@@ -17,11 +15,3 @@ export default function Home({
   );
 }
 
-// ref: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
-
-export async function getServerSideProps() {
-  const NEXT_PUBLIC_EXAMPLE_ENV_VAR = process.env.NEXT_PUBLIC_EXAMPLE_ENV_VAR;
-  const SECRET_KEY = process.env.SECRET_KEY;
-
-  return { props: { NEXT_PUBLIC_EXAMPLE_ENV_VAR, SECRET_KEY } };
-}
