261c571dab3e852c579b8f99558f9a2d828c80b8
romelgomez <bmxandcode@gmail.com>
2024-08-19 13:39:58 -0500

    chore: updat the env var name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 export default function Home() {
-  const NEXT_PUBLIC_EXAMPLE_API_KEY = process.env.NEXT_PUBLIC_EXAMPLE_API_KEY;
-  const SECRED_KEY = process.env.SECRED_KEY;
+  const NEXT_PUBLIC_EXAMPLE_ENV_VAR = process.env.NEXT_PUBLIC_EXAMPLE_ENV_VAR;
+  const SECRET_KEY = process.env.SECRET_KEY;
 
   return (
     <div>
       <h1>Experiment One</h1>
       <h3>Show/use env var from railway.app variable interface</h3>
 
-      <div>
-        NEXT_PUBLIC_EXAMPLE_API_KEY: {NEXT_PUBLIC_EXAMPLE_API_KEY}
-      </div>
+      <div>NEXT_PUBLIC_EXAMPLE_ENV_VAR: {NEXT_PUBLIC_EXAMPLE_ENV_VAR}</div>
       <br />
-      <div>
-        SECRED_KEY: {SECRED_KEY}
-      </div>
+      <div>SECRET_KEY: {SECRET_KEY}</div>
     </div>
   );
 }
