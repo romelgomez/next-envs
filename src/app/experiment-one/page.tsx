103ef484f27aedaebf871e4475d84326f4cb45d4
romelgomez <bmxandcode@gmail.com>
2024-08-19 12:07:25 -0500

    chore: add experiment-one

--- src/app/experiment-one/page.tsx
+++ src/app/experiment-one/page.tsx
@@ -1,15 +1,11 @@
-
-
 export default function Home() {
+  const NEXT_PUBLIC_EXAMPLE_API_KEY = process.env.NEXT_PUBLIC_EXAMPLE_API_KEY;
 
-  let NEXT_PUBLIC_EXAMPLE_API_KEY = process.env.NEXT_PUBLIC_EXAMPLE_API_KEY
-
-  return <div>
-    <h1>Experiment One</h1>
-
-    <h3>Show/use env var from railway.app variable interface</h3>
-
-    NEXT_PUBLIC_EXAMPLE_API_KEY: {NEXT_PUBLIC_EXAMPLE_API_KEY}
-
-  </div>
+  return (
+    <div>
+      <h1>Experiment One</h1>
+      <h3>Show/use env var from railway.app variable interface</h3>
+      NEXT_PUBLIC_EXAMPLE_API_KEY: {NEXT_PUBLIC_EXAMPLE_API_KEY}
+    </div>
+  );
 }
