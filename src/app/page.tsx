96e3c994b8c8a0571404c583accd98ac521159c4
romelgomez <bmxandcode@gmail.com>
2024-08-19 13:37:26 -0500

    chore: add secret key example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 export default function Home() {
   const NEXT_PUBLIC_EXAMPLE_API_KEY = process.env.NEXT_PUBLIC_EXAMPLE_API_KEY;
+  const SECRED_KEY = process.env.SECRED_KEY;
 
   return (
     <div>
       <h1>Experiment One</h1>
       <h3>Show/use env var from railway.app variable interface</h3>
-      NEXT_PUBLIC_EXAMPLE_API_KEY: {NEXT_PUBLIC_EXAMPLE_API_KEY}
+
+      <div>
+        NEXT_PUBLIC_EXAMPLE_API_KEY: {NEXT_PUBLIC_EXAMPLE_API_KEY}
+      </div>
+      <br />
+      <div>
+        SECRED_KEY: {SECRED_KEY}
+      </div>
     </div>
   );
 }
